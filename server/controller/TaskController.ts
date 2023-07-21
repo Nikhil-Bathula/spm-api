e5d7cd1b429853e2be3c98e0849d8a88725d6821
Yash Siddham <yashsiddham@gmail.com>
2023-07-21 13:28:33 -0400

    Stashing again

--- server/controller/TaskController.ts
+++ server/controller/TaskController.ts
@@ -7,29 +7,20 @@ export class TaskController {
     constructor() {
         this.taskRepo = new TaskRepository()
     }
-
     async getAssignedTasks(employee_id: number){
         return await this.taskRepo.getAssignedTasks(employee_id)
     }
-
     async createTask(req: Request, res: Response){
         // console.log(`DATA : ${{...req.body}}`)
         console.log(`DATA : ${req.body.name}`)
-
         try {
             console.log(`CALLING repo`)
             const tss_repo = await this.taskRepo.createTask(req.body)
-            console.log(`CALLED repo ${JSON.stringify(tss_repo?.status)}`)
-
+            // console.log(`CALLED repo ${JSON.stringify(tss_repo?.status)}`)
         } catch (err) {
             console.log(`ERR TSController: ${err}`)
             res.sendStatus(400)
         }
-
         res.sendStatus(201)
-
     }
-
-
-
 }
