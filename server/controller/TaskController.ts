3f08eb80b1d1a3cf62aa7b8f8fbe95250bd898d7
Nikhil Bathula <bathulanikhil98@gmail.com>
2023-08-13 19:52:36 -0400

    Removed Commented Code

--- server/controller/TaskController.ts
+++ server/controller/TaskController.ts
@@ -14,42 +14,12 @@ export class TaskController {
     return await this.taskRepo.getAssignedTasks(employee_id)
   }
   async createTask(req: Request, res: Response) {
-    // console.log(`DATA : ${{...req.body}}`)
-    console.log(`DATA : ${JSON.stringify(req.body)}`)
-    // const id = await this.prisma.task.findFirst({
-    //     select:{
-    //         id : true
-    //     }
-    // })
-    // console.log(`ID GOT : ${JSON.stringify(id)}`)
-    //
-    // const data = await this.prisma.task.create({
-    //     data: {
-    //         //
-    //         // name: req.body.name,
-    //         // description : req.body.description,
-    //         // status_id: req.body.status_id,
-    //         // project_id: req.body.project_id,
-    //         // employee_id: req.body.employee_id,
-    //         // assigned_to: req.body.assigned_to
-    //         ...req.body
-    //     }
-    // });
-    // console.log(`DATA CREATED : ${JSON.stringify(data)}`)
-    // console.log()
     try {
-      console.log(`INSIDE TRY - 24`)
-      res.status(201).json(await this.prisma.task.create({
+      return await this.prisma.task.create({
         data: {
           ...req.body
-          // name : "Task-NEW",
-          // status_id: 1,
-          // project_id: 1,
-          // employee_id: 1,
-          // assigned_to: 1,
-          // description: "desc"
         }
-      }))
+      })
     } catch (err) {
       console.log(`INSIDE CATCH - 39`)
       // @ts-ignore
@@ -91,15 +61,6 @@ export class TaskController {
         res.status(400).json({ "message": "Bad Request", "error": error.message })
       }
     }
-    //     try {
-    //         console.log(`CALLING repo`)
-    //         const tss_repo = await this.taskRepo.createTask(req.body)
-    //         // console.log(`CALLED repo ${JSON.stringify(tss_repo?.status)}`)
-    //     } catch (err) {
-    //         console.log(`ERR TSController: ${err}`)
-    //         res.sendStatus(400)
-    //     }
-    //     res.sendStatus(201)
   }
 
   async getTaskDetail(task_id: number) {
